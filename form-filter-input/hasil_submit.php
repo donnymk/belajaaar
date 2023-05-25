2ea01157ce1a330fa287045537416f89b8ff9a21
Donny Malik Kurniawan <bossdony@gmail.com>
2023-05-25 14:40:52 +0700

    Update hasil_submit.php

--- form-filter-input/hasil_submit.php
+++ form-filter-input/hasil_submit.php
@@ -2,12 +2,16 @@
 $koneksi = mysqli_connect("127.0.0.1","root","","bpsdmdjateng_daftar");
 
 // fungsi-fungsi untuk menampung variabel input dan melakukan sanitasi inputan
-$string__ = mysqli_real_escape_string($koneksi, $_POST['input1']);
+$string1 = filter_input(INPUT_POST, "input1", FILTER_SANITIZE_STRING);
+$string2 = htmlspecialchars($string1);
 
-$string_ = filter_input(INPUT_POST, "input1", FILTER_SANITIZE_STRING);
-$string = htmlspecialchars($string_);
-	
-echo 'Output fungsi mysqli_real_escape_string: '.$string__.'<br>';
-echo 'Output layer pertama fungsi filter_input: '.$string_.'<br>';
-echo 'Output layer kedua fungsi htmlspecialchars: '.$string;
+$string3 = mysqli_real_escape_string($koneksi, $_POST['input1']);
+$string4 = mysqli_real_escape_string($koneksi, $string1);;
+
+
+echo 'Output fungsi filter_input: '.$string1.'<br>';
+echo 'Output fungsi filter_input + htmlspecialchars: '.$string2.'<br><br>';
+
+echo 'Output fungsi mysqli_real_escape_string: '.$string3.'<br>';
+echo 'Output fungsi filter_input + mysqli_real_escape_string: '.$string4.'<br>';
 ?>
